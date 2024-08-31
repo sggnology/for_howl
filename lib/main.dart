f3476c5af0f692948bf998e3b3a94edf95ec06ef
sggnology <sggnology@gmail.com>
2024-08-31 18:52:12 +0900

    [service] App 에서 SharedPreference 사용할 수 있게끔 시작시 초기화할 수 있게끔 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:for_howl/service/shared_preference/SharedPreferenceService.dart';
 import 'package:for_howl/view/playlist/index.dart';
-import 'package:for_howl/view/profile/index.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SharedPreferencesService().init();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
