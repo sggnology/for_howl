4d8eb6445a552c6184feccbf1692973306d4f71e
sggnology <sggnology@gmail.com>
2024-10-06 21:40:16 +0900

    [service] MyAudioHandler region 설정
    - 가독성을 높이기 위해 생성자 region 을 설정

--- lib/service/audio_handler/MyAudioHandler.dart
+++ lib/service/audio_handler/MyAudioHandler.dart
@@ -5,7 +5,6 @@ import 'package:just_audio/just_audio.dart';
 import 'model/YoutubeAudioMetaInfo.dart';
 
 class MyAudioHandler extends BaseAudioHandler {
-
   static MyAudioHandler _instance = MyAudioHandler._internal();
 
   factory MyAudioHandler() {
@@ -14,49 +13,13 @@ class MyAudioHandler extends BaseAudioHandler {
 
   late AudioPlayer _audioPlayer;
 
+  //region Constructor & Helper of Constructor
   MyAudioHandler._internal() {
     _audioPlayer = AudioPlayer();
 
     _audioPlayer.playbackEventStream.listen((event) {
       playbackState.add(_transformEvent(event));
     });
-  }
-
-  Future<void> init() async {
-    _instance = await AudioService.init(
-      builder: () => MyAudioHandler(),
-      config: const AudioServiceConfig(
-        androidNotificationChannelId: 'com.example.app.channel.audio',
-        androidNotificationChannelName: 'Audio Playback',
-        androidNotificationOngoing: true,
-      ),
-    );
-  }
-
-  Future<void> setAudioSource(String url) async {
-
-    YoutubeAudioMetaInfo youtubeAudioMetaInfo = await YoutubeHelper.extractAudioMetaInfoFromURL(url);
-
-    // 오디오 스트림을 설정합니다.
-    Duration? audioDuration = await _instance._audioPlayer.setUrl(youtubeAudioMetaInfo.url);
-
-    final currentMediaItem = mediaItem.valueOrNull;
-
-    // MediaItem 중복 확인
-    // - 중복되는 값에 대해 UI 업데이트를 방지하기 위함
-    if (currentMediaItem == null || currentMediaItem.id != url) {
-
-      Map<String, dynamic> map = <String, dynamic>{};
-      map.putIfAbsent("audioDuration", () => audioDuration?.inSeconds ?? Duration.zero.inSeconds);
-
-      mediaItem.add(MediaItem(
-        id: youtubeAudioMetaInfo.url,
-        title: youtubeAudioMetaInfo.title,
-        artist: youtubeAudioMetaInfo.artist,
-        duration: _audioPlayer.duration,
-        extras: map
-      ));
-    }
   }
 
   static PlaybackState _transformEvent(PlaybackEvent event) {
@@ -67,7 +30,8 @@ class MyAudioHandler extends BaseAudioHandler {
         MediaControl.stop,
       ],
       androidCompactActionIndices: const [0, 1, 2],
-      processingState: _convertProcessingState(_instance._audioPlayer.processingState),
+      processingState:
+          _convertProcessingState(_instance._audioPlayer.processingState),
       playing: _instance._audioPlayer.playing,
       updatePosition: _instance._audioPlayer.position,
       bufferedPosition: _instance._audioPlayer.bufferedPosition,
@@ -94,6 +58,45 @@ class MyAudioHandler extends BaseAudioHandler {
         return AudioProcessingState.completed;
       default:
         throw Exception("Invalid state: $state");
+    }
+  }
+  //endregion
+
+  Future<void> init() async {
+    _instance = await AudioService.init(
+      builder: () => MyAudioHandler(),
+      config: const AudioServiceConfig(
+        androidNotificationChannelId: 'com.example.app.channel.audio',
+        androidNotificationChannelName: 'Audio Playback',
+        androidNotificationOngoing: true,
+      ),
+    );
+  }
+
+  Future<void> setAudioSource(String url) async {
+    YoutubeAudioMetaInfo youtubeAudioMetaInfo =
+        await YoutubeHelper.extractAudioMetaInfoFromURL(url);
+
+    // 오디오 스트림을 설정합니다.
+    Duration? audioDuration = await _instance
+        ._audioPlayer
+        .setUrl(youtubeAudioMetaInfo.url);
+
+    final currentMediaItem = mediaItem.valueOrNull;
+
+    // MediaItem 중복 확인
+    // - 중복되는 값에 대해 UI 업데이트를 방지하기 위함
+    if (currentMediaItem == null || currentMediaItem.id != url) {
+      Map<String, dynamic> map = <String, dynamic>{};
+      map.putIfAbsent("audioDuration",
+          () => audioDuration?.inSeconds ?? Duration.zero.inSeconds);
+
+      mediaItem.add(MediaItem(
+          id: youtubeAudioMetaInfo.url,
+          title: youtubeAudioMetaInfo.title,
+          artist: youtubeAudioMetaInfo.artist,
+          duration: _audioPlayer.duration,
+          extras: map));
     }
   }
 
