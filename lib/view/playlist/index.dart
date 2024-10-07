da8a07749f8a5f5cf55f0463936bb5d0add59875
sggnology <sggnology@gmail.com>
2024-10-07 22:58:45 +0900

    [view] DatePicker 추가 및 playlist 상단에 추가

--- lib/view/playlist/index.dart
+++ lib/view/playlist/index.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:for_howl/service/audio_handler/MyAudioHandler.dart';
 import 'package:for_howl/view/component/audio_player_screen/AudioPlayerScreen.dart';
+import 'package:for_howl/view/component/date_picker/DatePicker.dart';
 
 class PlaylistPage extends StatelessWidget {
-
   PlaylistPage({
     super.key,
   });
@@ -16,17 +16,23 @@ class PlaylistPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('PlayList Page'),
       ),
-      body: const Row(
+      body: const Column(
         children: [
-          Flexible(
-            fit: FlexFit.tight,
+          DatePicker(),
+          Expanded(
+            flex: 1,
             child: AudioPlayerScreen(),
           ),
+          // const Expanded(
+          //   flex: 1,
+          //   child: Text('hi'),
+          // ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _audioHandler.setAudioSource("https://www.youtube.com/watch?v=6RQ-bBdASvk");
+          _audioHandler
+              .setAudioSource("https://www.youtube.com/watch?v=6RQ-bBdASvk");
           // Add your onPressed code here!
         },
         child: const Icon(Icons.add),
